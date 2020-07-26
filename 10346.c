d8120ff675b9da8605b332ce42da1f54f343b591
mahede <mahede.object@gmail.com>
2020-07-26 11:17:15 +0600

    Peter's Smokes

---
+++ 10346.c
@@ -0,0 +1,23 @@
+// problem 10346 peter smooke
+/* 
+
+* n cigarrete, keeping all,k>1 then he replace a new cigarrete
+
+ */
+#include<stdio.h>
+int main(){
+    int n, k, total;
+    while(scanf("%d%d")==2){
+        total = n;
+
+        while (n>=k)
+        {
+            int renew = n/k;
+            total = total + renew;
+
+            n=renew + (n%k);
+        }
+        printf("%d\n",total);
+    }
+    return 0;
+}
